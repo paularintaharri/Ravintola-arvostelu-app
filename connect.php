836aaee765e0451f082b8bab749a7b52fa64fcd8
Johanna T <johanna.tani@metropolia.fi>
2016-12-01 11:15:05 +0000

    luotu uusia kansioita (ei välttämättä käytetä(esimerkkikarttahomma))
    ;
    )

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+    // Includes username etc. information
+
+    // $servername = getenv('IP');
+    //$username = getenv('C9_USER');
+    //$dbport = 3306;
+     
+    $servername = 'localhost';
+    $username = 'johannrt';
+    $password = "";
+    $database = "Projekti";
+  
+
+    // Create connection
+ /*   $db = new mysqli($servername, $username, $password, $database, $dbport);
+
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    } 
+    echo "Connected successfully (".$db->host_info.")"; */
+?>    

6ab071e5156e88f4aa85683fd3eeded1bfb04dc1
Paula R <paula.rinta-harri@metropolia.fi>
2016-12-12 12:21:47 +0000

    tehty muutoksia

--- connect.php
+++ connect.php
@@ -1,22 +1,32 @@
 <?php
-    // Includes username etc. information
-
-    // $servername = getenv('IP');
-    //$username = getenv('C9_USER');
-    //$dbport = 3306;
-     
-    $servername = 'localhost';
-    $username = 'johannrt';
+$servername = 'localhost';
+    $username = 'root';
     $password = "";
-    $database = "Projekti";
+    $database = "restaurant";
   
 
     // Create connection
- /*   $db = new mysqli($servername, $username, $password, $database, $dbport);
+    $link = new mysqli($servername, $username, $password, $database);
 
     // Check connection
-    if ($db->connect_error) {
-        die("Connection failed: " . $db->connect_error);
+    if ($link->connect_error) {
+        die("Connection failed: " . $link->connect_error);
     } 
-    echo "Connected successfully (".$db->host_info.")"; */
+    echo "Connected successfully (".$link->host_info.")"; 
+    
+    $sql = "SELECT * FROM markers";
+    $result = mysqli_query($link, $sql);
+    //$result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Address: " . $row["address"].
+            "Cordinates: ". $row["lat"]. " ". $row["lng"]. " ". $row["type"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $link->close();
 ?>    
